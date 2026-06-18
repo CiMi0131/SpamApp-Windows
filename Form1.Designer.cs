d5978a2cee24f492ab601726ca5c7ed119d204ec
CiMi131 <121714720+CiMi0131@users.noreply.github.com>
2026-06-18 15:14:30 +0300

    Proje Dosyaları [C# Windows Forms]

---
+++ Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace Spam_App
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            label1 = new Label();
+            timer1 = new System.Windows.Forms.Timer(components);
+            label2 = new Label();
+            checkBox1 = new CheckBox();
+            textBox1 = new TextBox();
+            label3 = new Label();
+            textBox2 = new TextBox();
+            checkBoxEnter = new CheckBox();
+            textBox3 = new TextBox();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Consolas", 20.25F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.FromArgb(230, 230, 230);
+            label1.Location = new Point(119, 29);
+            label1.Name = "label1";
+            label1.Size = new Size(149, 32);
+            label1.TabIndex = 1;
+            label1.Text = "SPAM MODE";
+            // 
+            // timer1
+            // 
+            timer1.Interval = 1;
+            timer1.Tick += timer1_Tick;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Consolas", 12.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label2.Location = new Point(250, 93);
+            label2.Name = "label2";
+            label2.Size = new Size(99, 20);
+            label2.TabIndex = 3;
+            label2.Text = "Durduruldu";
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Font = new Font("Consolas", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            checkBox1.Location = new Point(12, 75);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(187, 46);
+            checkBox1.TabIndex = 4;
+            checkBox1.Text = "\r\nAçmak için sol tıkla\r\nKapatmak için sağ tıkla\r\n";
+            checkBox1.UseVisualStyleBackColor = true;
+            checkBox1.CheckedChanged += checkBox1_CheckedChanged;
+            checkBox1.MouseDown += checkBox1_MouseDown;
+            // 
+            // textBox1
+            // 
+            textBox1.BackColor = Color.FromArgb(75, 75, 75);
+            textBox1.BorderStyle = BorderStyle.None;
+            textBox1.Font = new Font("Consolas", 9F);
+            textBox1.ForeColor = Color.FromArgb(230, 230, 230);
+            textBox1.Location = new Point(131, 147);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.PlaceholderText = "örnek : a";
+            textBox1.Size = new Size(249, 63);
+            textBox1.TabIndex = 5;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Consolas", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label3.Location = new Point(12, 149);
+            label3.Name = "label3";
+            label3.Size = new Size(133, 168);
+            label3.TabIndex = 6;
+            label3.Text = "YAZILACAK YAZI :\r\n\r\n\r\n\r\n\r\nMS BAŞINA SPAM :\r\n\r\nSPAM LİMİTİ :\r\n\r\nBASILACAK TUŞLAR :\r\n\r\nHOTKEY :    F6";
+            
+            // 
+            // textBox2
+            // 
+            textBox2.BackColor = Color.FromArgb(75, 75, 75);
+            textBox2.BorderStyle = BorderStyle.None;
+            textBox2.Font = new Font("Consolas", 9F);
+            textBox2.ForeColor = Color.FromArgb(230, 230, 230);
+            textBox2.Location = new Point(131, 222);
+            textBox2.Name = "textBox2";
+            textBox2.PlaceholderText = "1sn 1000ms dir";
+            textBox2.Size = new Size(249, 15);
+            textBox2.TabIndex = 7;
+            textBox2.TextChanged += textBox2_TextChanged;
+            // 
+            // checkBoxEnter
+            // 
+            checkBoxEnter.AutoSize = true;
+            checkBoxEnter.Checked = true;
+            checkBoxEnter.CheckState = CheckState.Checked;
+            checkBoxEnter.Font = new Font("Consolas", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            checkBoxEnter.Location = new Point(146, 274);
+            checkBoxEnter.Name = "checkBoxEnter";
+            checkBoxEnter.Size = new Size(61, 18);
+            checkBoxEnter.TabIndex = 9;
+            checkBoxEnter.Text = "ENTER";
+            checkBoxEnter.UseVisualStyleBackColor = true;
+            // 
+            // textBox3
+            // 
+            textBox3.BackColor = Color.FromArgb(75, 75, 75);
+            textBox3.BorderStyle = BorderStyle.None;
+            textBox3.Font = new Font("Consolas", 9F);
+            textBox3.ForeColor = Color.FromArgb(230, 230, 230);
+            textBox3.Location = new Point(131, 249);
+            textBox3.Name = "textBox3";
+            textBox3.PlaceholderText = "örnek: 500 (opsiyonel)";
+            textBox3.Size = new Size(249, 15);
+            textBox3.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(26, 26, 26);
+            ClientSize = new Size(390, 474);
+            Controls.Add(textBox3);
+            Controls.Add(checkBoxEnter);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(checkBox1);
+            Controls.Add(label3);
+            ForeColor = Color.FromArgb(230, 230, 230);
+            MaximizeBox = false;
+            MaximumSize = new Size(406, 513);
+            MinimizeBox = false;
+            MinimumSize = new Size(406, 513);
+            Name = "Form1";
+            ShowIcon = false;
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "SPAM MODE - Made by CiMi131";
+            Load += Form1_Load;
+           
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private Label label2;
+        private CheckBox checkBox1;
+        private TextBox textBox1;
+        private Label label3;
+        private TextBox textBox2;
+        private CheckBox checkBoxEnter;
+        private TextBox textBox3;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Spam_App
+{
+    public partial class Form1 : Form
+    {
+        // Windows API Importları (Global Hotkey İçin)
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern bool RegisterHotKey(IntPtr hWnd, int id, int fsModifiers, int vk);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+
+        private const int HOTKEY_ID = 1;
+        private const int WM_HOTKEY = 0x0312;
+
+        // Durum Değişkenleri
+        private int geriSayim = 3;
+        private int spamSayaci = 0;
+        private int spamLimiti = 0;
+        private bool isHotKeyRegistered = false;
+
+        // Timer nesnesi
+        private System.Windows.Forms.Timer geriSayimTimer;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Timer'ı güvenli şekilde başlatıyoruz
+            geriSayimTimer = new System.Windows.Forms.Timer();
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            // Global Hotkey tetiklendiğinde (F6) CheckBox durumunu tersine çevirir
+            if (m.Msg == WM_HOTKEY && m.WParam.ToInt32() == HOTKEY_ID)
+            {
+                checkBox1.Checked = !checkBox1.Checked;
+            }
+            base.WndProc(ref m);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            geriSayimTimer.Interval = 1000;
+            geriSayimTimer.Tick += GeriSayimTimer_Tick;
+
+            // F6 tuşunu uygulamaya kaydet
+            isHotKeyRegistered = RegisterHotKey(this.Handle, HOTKEY_ID, 0, (int)Keys.F6);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            string text = textBox1.Text;
+
+            if (!string.IsNullOrEmpty(text))
+            {
+                try
+                {
+                    // Özel karakterleri güvenli hale getirip direkt gönderiyoruz
+                    string safeText = EncodeSendKeysText(text);
+                    SendKeys.Send(safeText);
+                }
+                catch
+                {
+                    // Gönderim sırasında oluşabilecek anlık odak kayıplarını absorbe etmek için
+                }
+            }
+
+            // Eğer enter seçeneği aktifse metnin arkasından ENTER tuşu basar
+            if (checkBoxEnter.Checked)
+            {
+                SendKeys.Send("{ENTER}");
+            }
+
+            // Limit Kontrolü
+            if (spamLimiti > 0)
+            {
+                spamSayaci++;
+                if (spamSayaci >= spamLimiti)
+                {
+                    StopSpam("Limit doldu.");
+                }
+            }
+        }
+
+        // SendKeys için özel karakterleri kaçıran (escape) güvenli metot
+        private string EncodeSendKeysText(string input)
+        {
+            if (string.IsNullOrEmpty(input)) return input;
+
+            StringBuilder sb = new StringBuilder();
+            foreach (char c in input)
+            {
+                // SendKeys için özel anlam ifade eden karakterler süslü paranteze alınır
+                if (c == '+' || c == '^' || c == '%' || c == '~' ||
+                    c == '(' || c == ')' || c == '{' || c == '}' ||
+                    c == '[' || c == ']')
+                {
+                    sb.Append($"{{{c}}}");
+                }
+                else
+                {
+                    sb.Append(c);
+                }
+            }
+            return sb.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            checkBox1.Checked = true;
+        }
+
+        private void GeriSayimTimer_Tick(object sender, EventArgs e)
+        {
+            if (geriSayim > 0)
+            {
+                label2.Text = $"{geriSayim}...";
+                geriSayim--;
+            }
+            else
+            {
+                geriSayimTimer.Stop();
+                geriSayim = 3;
+                spamSayaci = 0;
+
+                timer1.Start();
+                label2.Text = "ÇALIŞIYOR!";
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                if (string.IsNullOrWhiteSpace(textBox2.Text) || string.IsNullOrWhiteSpace(textBox1.Text))
+                {
+                    checkBox1.Checked = false;
+                    MessageBox.Show("Değer boş olamaz", "Spam App", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+
+                if (!int.TryParse(textBox3.Text, out spamLimiti) || spamLimiti < 0)
+                {
+                    spamLimiti = 0;
+                }
+
+                ToggleUI(false);
+                geriSayim = 3;
+                label2.Text = "3...";
+                geriSayimTimer.Start();
+            }
+            else
+            {
+                StopSpam("Durduruldu.");
+            }
+        }
+
+        private void StopSpam(string message)
+        {
+            geriSayimTimer.Stop();
+            timer1.Stop();
+            geriSayim = 3;
+            spamSayaci = 0;
+
+            label2.Text = message;
+            checkBox1.Checked = false;
+            ToggleUI(true);
+        }
+
+        private void ToggleUI(bool enable)
+        {
+            textBox1.Enabled = enable;
+            textBox2.Enabled = enable;
+            textBox3.Enabled = enable;
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            if (int.TryParse(textBox2.Text, out int interval) && interval > 0)
+            {
+                timer1.Interval = interval;
+            }
+        }
+
+        private void checkBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                checkBox1.Checked = false;
+            }
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            if (isHotKeyRegistered)
+            {
+                UnregisterHotKey(this.Handle, HOTKEY_ID);
+            }
+            base.OnFormClosing(e);
+        }
+    }
+}
